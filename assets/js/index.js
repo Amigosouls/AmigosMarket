0589d98750f682f2430f049eca4d52c0687f1b99
Amigosouls <amigosouls022@gmail.com>
2023-05-19 15:33:15 +0530

    amigosmarket 4th commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,32 +21,6 @@
 })()
 
 
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-
-const alertBox = (msg) => {
-    const wrapper = document.createElement('div')
-    wrapper.innerHTML = [
-        `<div class="alert alert-success alert-dismissible" role="alert">`,
-        `<h3>${msg}</h3>`,
-        `<button type="button" class="btn btn-secondary form-btn" data-bs-dismiss="alert" aria-label="Close">Cancel</button>`,
-        '</div>'
-    ].join('')
-
-    alertPlaceholder.append(wrapper)
-}
-
-// import { restClient } from '/node_modules/@polygon.io/client-js'
-// const rest = restClient("sF98gBtWzFtPZwy_lDORd2RSb4s08cIq");
-
-// rest.stocks.aggregates("AAPL", 1, "day", "2023-01-01", "2023-04-14").then((data) => {
-// 	console.log(data);
-// }).catch(e => {
-// 	console.error('An error happened:', e);
-// });
-
-// console.log(aggregates)
-
-
 
 function signUpUser() {
     const firstName = document.getElementById('fname').value;
@@ -70,7 +44,27 @@ function signUpUser() {
     alertBox("Account Create Successfully");
 }
 
-
+function userLogin(){
+    const userName = document.getElementById('log_uname').value;
+    const userpass = document.getElementById('log_pass').value;
+    const xmlParser = new XMLHttpRequest();
+    xmlParser.open("GET", "http://localhost:3000/Users");
+    xmlParser.send();
+    xmlParser.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const jsonData = JSON.parse(this.responseText);
+            for (let values of jsonData) {
+                if(userName==values['uname'] && userpass==values['password'])
+                {
+                    console.log("success");
+                    alertBox("Success")
+                }
+            }
+           
+        }
+        console.log("loginFailed");
+    }
+}
 
 
 $(document).ready(function(){
@@ -98,16 +92,35 @@ $(document).ready(function(){
         }
     })
   });
+  var count =0;
 
-  function getStockValue(){
-    fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey=sF98gBtWzFtPZwy_lDORd2RSb4s08cIq`,{method:"GET"  //api link from pixaby
+// function to update stock market shares
+
+//   setInterval(function(){
+    
+//     const comp_list =["A","AA","AAC","AACI","AADI","AADI","AAIC","AAL","AAL","AAN","AAPL","ABC","ABG"];
+//     getStockValue(comp_list[count]);    
+//         if(count==12){
+//             count=0;
+//         }
+//   },15000);
+
+  function getStockValue(comp_name){
+        var stock_string = ""
+        stock_string+=`<p>`
+        count+=1;
+    fetch(`https://api.polygon.io/v2/aggs/ticker/${comp_name}/range/1/day/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey=sF98gBtWzFtPZwy_lDORd2RSb4s08cIq`,{method:"GET"  //api link from pixaby
     }).then(res => res.json())
     .then(stock=>{
-        console.log(stock.ticker)
-        console.log(stock.results[0])
-        
+            stock_string+=`<h5> Company Name:${stock.ticker}</h5>`;
+            stock_string+=`<h5> Close Price:${stock.results[0].c}</h5>`;
+            stock_string+=`<h5> Highest Price:${stock.results[0].h}</h5>`;
+            stock_string+=`<h5> Lowest Price Price:${stock.results[0].l}</h5>`;
+            stock_string+=`<h5> Number of transactions:${stock.results[0].n}</h5>`;
+            stock_string+=`<h5>Trading Volume:${stock.results[0].v}</h5>`;
+            stock_string+=`</p>`
+            $(".marketNews").html(stock_string);
     })
+    
 }
-getStockValue()
-
 
